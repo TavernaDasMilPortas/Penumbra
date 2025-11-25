3496d994553cfcc379478c00dcb2aa839c00177c
giuliauri <giulia.urias@hotmail.com>
2025-11-25 00:15:54 -0300

    Refactor scene names and update MainMenu logic
    Renamed CasaIdle.unity to HouseIdle.unity and removed HouseIdleScene.unity. Updated MainMenu.cs to use the new scene name and improved cursor handling. Added new logo asset and Showcase.unity scene. Updated EditorBuildSettings to reflect these changes.

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -4,14 +4,36 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
-{ 
+{
+    
+    [SerializeField] private string houseSceneName = "HouseIdle";
+
+    void Start()
+    {
+        
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+
+        
+        SceneManager.LoadScene(houseSceneName, LoadSceneMode.Additive);
+    }
+
     public void PlayGame()
     {
+        
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
+        
         SceneManager.LoadSceneAsync("MainGame");
     }
 
     public void QuitGame()
     {
         Application.Quit();
+        
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
     }
 }
